c428dfa910d944e7a88173be19255baca1da912a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-27 21:11:12 +0200

    implement control for ws

--- server/db.js
+++ server/db.js
@@ -1,12 +1,13 @@
-// Database MySQL
-const mysql2 = require('mysql2');
+import mysql2 from 'mysql2';
+import sqlite3 from 'sqlite3';
+
 let dbmysql = null;
 
 function openMysql(name) {
   dbmysql = mysql2.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123456',
+    password: '777',
     database: name,
   });
   dbmysql.connect((err) => {
@@ -19,11 +20,12 @@ function openMysql(name) {
   return dbmysql;
 }
 
-const sqlite3 = require('sqlite3').verbose();
+const sql = sqlite3.verbose();
+// console.log('sql: ', sql);
 let db = null;
 
 function open(name) {
-  db = new sqlite3.Database('../build/' + name + '.sqlite', (err) => {
+  db = new sql.Database('server/' + name + '.db', (err) => {
     if (err) console.error(err.message);
     else console.log("Database '" + name + "' is open!");
   });
@@ -34,3 +36,89 @@ function open(name) {
 function close() {
   db.close();
 }
+
+function control(ws) {
+  ws.on('message', function (message) {
+    try {
+      const { type, data } = JSON.parse(message);
+
+      if (type === 'insert') insert(data.tableName, data.placeholders, data.values);
+      else if (type === 'update')
+        update(data.tableName, data.placeholders, data.values, data.where);
+      else if (type === 'select') {
+        select(data.tableName, data.placeholders, data.where).then((rows) => {
+          ws.send(
+            JSON.stringify({
+              type: 'databaseSelectChanged',
+              tableName: data.tableName,
+              value: rows,
+            })
+          );
+        });
+      } else if (type === 'selectAll') {
+        const sql = data.where ? ` WHERE ${data.where}` : '';
+        db.all('SELECT * FROM ' + data.tableName + sql, (err, rows) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          ws.send(
+            JSON.stringify({
+              type: 'databaseSelectAllChanged',
+              tableName: data.tableName,
+              value: rows,
+            })
+          );
+        });
+      } else if (type === 'selectAllTwo') {
+        db.all('SELECT * FROM ' + data.tableName, (err, rows) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          ws.send(
+            JSON.stringify({
+              type: 'databaseSelectAllTwoChanged',
+              tableName: data.tableName,
+              value: rows,
+            })
+          );
+        });
+      } else if (type === 'selectAllunit') {
+        db.all('SELECT * FROM ' + data.tableName, (err, rows) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          ws.send(
+            JSON.stringify({
+              type: 'databaseSelectAllunitChanged',
+              tableName: data.tableName,
+              value: rows,
+            })
+          );
+        });
+      } else if (type === 'selectSorted') {
+        select(
+          data.tableName,
+          data.placeholders,
+          data.where,
+          data.orderBy,
+          data.orderDirection
+        ).then((rows) => {
+          ws.send(
+            JSON.stringify({
+              type: 'databaseSelectSortedChanged',
+              tableName: data.tableName,
+              value: rows,
+            })
+          );
+        });
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  });
+}
+
+export { open, close, openMysql, control };
