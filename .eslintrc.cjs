86fc0ae4c10d5467374c70a4481e1076f6388a39
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-27 21:09:37 +0200

    install cors, update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,16 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
+    // 'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: '2020', sourceType: 'script' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-  },
-  globals: {
-    require: true,
-    module: true,
   },
 };
