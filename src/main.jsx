82ec55fb702c00e07ddae792114572ba3d4dae79
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-11-29 20:45:25 +0200

    Refactor the WebSocket management code to handle the connection asynchronously using promises.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+// import { connectWebsocket } from './websocketManager.js';
+import GlobalStates from './shared/context/global.states.context.jsx';
+
+// Websocket
+const ws = new WebSocket('ws://localhost:5000');
+
+ws.onopen = () => {
+  console.log('Connected to the WebSocketServer at url: ' + ws.url);
+  console.log('ws: ', ws);
+
+  const message = {
+    type: 'debug',
+    msg: 'Proxe is connected to WebSocket',
+  };
+  ws.send(JSON.stringify(message));
+};
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStates ws={ws}>
+      <App />
+    </GlobalStates>
+  </React.StrictMode>
+);
+
+// connectWebsocket('ws://localhost:5000')
+//   .then((ws) => {
+//     ReactDOM.createRoot(document.getElementById('root')).render(
+//       <React.StrictMode>
+//         <GlobalStates ws={ws}>
+//           <App />
+//         </GlobalStates>
+//       </React.StrictMode>
+//     );
+//   })
+//   .catch((error) => {
+//     console.error('Error connecting to WebSocket:', error);
+//   });
--- src/websocketManager.js
+++ src/websocketManager.js
@@ -1,6 +1,6 @@
 let ws;
 
-const connectWebsocket = (url) => {
+export const connectWebsocket = (url) => {
   if (!url) {
     console.error('WebSocket URL is required');
     return;
@@ -8,7 +8,42 @@ const connectWebsocket = (url) => {
 
   ws = new WebSocket(url);
 
-  ws.onopen = () => {
-    console.log('Connected to the WebSocketServer at url: ' + url);
-  };
+  const onOpenPromise = new Promise((resolve) => {
+    ws.onopen = () => {
+      console.log('Connected to the WebSocketServer at url: ' + url);
+      console.log('ws: ', ws);
+
+      const message = {
+        type: 'debug',
+        msg: 'Proxe is connected to WebSocket',
+      };
+      ws.send(JSON.stringify(message));
+
+      resolve(ws);
+    };
+  });
+
+  return onOpenPromise;
 };
+
+// let ws;
+
+// export const connectWebsocket = (url) => {
+//   if (!url) {
+//     console.error('WebSocket URL is required');
+//     return;
+//   }
+
+//   ws = new WebSocket(url);
+
+//   ws.onopen = () => {
+//     console.log('Connected to the WebSocketServer at url: ' + url);
+//     console.log('ws: ', ws);
+
+//     const message = {
+//       type: 'debug',
+//       msg: 'Proxe is connected to WebSocket',
+//     };
+//     ws.send(JSON.stringify(message));
+//   };
+// };
