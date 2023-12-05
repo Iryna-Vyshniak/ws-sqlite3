624fd6268b832b01862b49580e4462ba479bd11e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-12-05 21:17:50 +0200

    add state of theme

--- src/shared/context/global.states.context.jsx
+++ src/shared/context/global.states.context.jsx
@@ -1,23 +1,33 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { connectWebsocket } from '../../websocketManager';
 
-const Context = createContext();
+export const Context = createContext();
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const useGlobalStates = () => {
   const context = useContext(Context);
 
   if (!context) {
-    throw new Error('useWebsocket must be used within a WebsocketProvider');
+    throw new Error('please, use Provider');
   }
   return context;
 };
 
 // eslint-disable-next-line react/prop-types
 const GlobalStates = ({ ws, children }) => {
+  const [theme, setCurrentTheme] = useState(() => localStorage.getItem('theme') || 'dark');
+
+  const toggleTheme = () => {
+    const newTheme = theme === 'dark' ? 'light' : 'dark';
+    setCurrentTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+  };
+
   const contextValue = {
     ws,
     connectWebsocket,
+    theme,
+    toggleTheme,
   };
 
   return <Context.Provider value={contextValue}>{children}</Context.Provider>;
