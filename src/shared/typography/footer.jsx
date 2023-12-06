908937bcfa4879116794636e161e672c5450ec1c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-12-06 20:20:00 +0200

    implement footer

---
+++ src/shared/typography/footer.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import { Row } from './base';
+import { vspx } from './viewsize';
+
+const Footer = ({ children, height, ...props }) => {
+  return (
+    <Row
+      height={height}
+      alignItems='center'
+      justifyContent='center'
+      padding={vspx(10)}
+      style={props}
+    >
+      {children}
+    </Row>
+  );
+};
+
+export default Footer;
