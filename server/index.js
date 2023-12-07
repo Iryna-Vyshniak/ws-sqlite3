40f0c017cf8da6473c370a64e1c3897ea8651fa7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-12-07 18:49:01 +0200

    insert complete values for sample projects in a more structured manner

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import { createServer } from 'http';
 import { WebSocketServer } from 'ws';
 import * as accManager from './account_manager.js';
 import * as dbManager from './db.js';
+// console.log('dbManager: ', dbManager);
 
 // Routing
 const app = express();
@@ -34,7 +35,9 @@ const tableCheckQuery = `SELECT name FROM sqlite_master WHERE type='table' AND n
 const createTableQuery = `CREATE TABLE IF NOT EXISTS projects (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   name VARCHAR(255) NOT NULL, 
-  orgname VARCHAR(255) NOT NULL);`;
+  orgname VARCHAR(255) NOT NULL,
+  slogan TEXT,
+  datacreate DATE);`;
 
 db.serialize(() => {
   db.all(tableCheckQuery, (err, rows) => {
@@ -50,11 +53,32 @@ db.serialize(() => {
           console.log('Table didn`t create');
         } else {
           // Table just created, creating some rows
-          const insert = 'INSERT INTO projects (name, orgname) VALUES (?,?)';
-          db.run(insert, ['Sophia Garcia', 'RST Group']);
-          db.run(insert, ['Daniel Martinez', 'UVW Corporation']);
-          db.run(insert, ['Olivia Rodriguez', 'HIJ Enterprises']);
-          db.run(insert, ['Alexander Nguyen', 'MNO Inc.']);
+          const insert =
+            'INSERT INTO projects (name, orgname, slogan, datacreate) VALUES (?, ?, ?, ?)';
+          db.run(insert, [
+            'Sophia Garcia',
+            'RST Group',
+            'Connecting Dreams',
+            new Date().toISOString().split('T')[0],
+          ]);
+          db.run(insert, [
+            'Daniel Martinez',
+            'UVW Corporation',
+            'Innovate to Elevate',
+            new Date().toISOString().split('T')[0],
+          ]);
+          db.run(insert, [
+            'Olivia Rodriguez',
+            'HIJ Enterprises',
+            'Empowering Excellence',
+            new Date().toISOString().split('T')[0],
+          ]);
+          db.run(insert, [
+            'Alexander Nguyen',
+            'MNO Inc.',
+            'Inspiring Tomorrow',
+            new Date().toISOString().split('T')[0],
+          ]);
         }
       });
       console.log('Projects table is successfully created.');
@@ -65,12 +89,12 @@ db.serialize(() => {
 });
 
 // Account
-accManager.setDatabase(db);
+// accManager.setDatabase(db);
 
 wss.on('connection', (ws) => {
   console.log('connecting to  WebSocket');
 
-  accManager.control(ws);
+  // accManager.control(ws);
   dbManager.control(ws);
 
   ws.on('message', (message) => {
