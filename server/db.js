259b7c1a487665040cc93ed8f2acf71df96b4b46
iryna.vyshniak@gmail.com <fletchertorn@gmail.com>
2023-12-09 19:35:40 +0200

    update db

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
 import sqlite3 from 'sqlite3';
 import insert from './controllers/insert.js';
 import update from './controllers/update.js';
+import deleteData from './controllers/delete.js';
 
 const sql = sqlite3.verbose();
 let db = null;
@@ -19,11 +20,9 @@ const close = () => {
 };
 
 const control = (ws) => {
-  // console.log('ws: ', ws);
   ws.on('message', (message) => {
     try {
       const { type, data } = JSON.parse(message);
-      // console.log('type, data: ', type, data);
 
       switch (type) {
         case 'insert':
@@ -31,6 +30,9 @@ const control = (ws) => {
           break;
         case 'update':
           update(data.tableName, data.placeholders, data.values, data.where, db);
+          break;
+        case 'delete':
+          deleteData(data.tableName, data.where, db);
           break;
         case 'select':
           select(data.tableName, data.placeholders, data.where).then((rows) => {
@@ -83,87 +85,5 @@ const control = (ws) => {
   });
 };
 
-// function control(ws) {
-//   ws.on('message', function (message) {
-//     try {
-//       const { type, data } = JSON.parse(message);
-//       if (type === 'insert') insert(data.tableName, data.placeholders, data.values, db);
-//       else if (type === 'update')
-//         update(data.tableName, data.placeholders, data.values, data.where, db);
-//       else if (type === 'select') {
-//         select(data.tableName, data.placeholders, data.where).then((rows) => {
-//           ws.send(
-//             JSON.stringify({
-//               type: 'databaseSelectChanged',
-//               tableName: data.tableName,
-//               value: rows,
-//             })
-//           );
-//         });
-//       } else if (type === 'selectAll') {
-//         const query = data.where ? ` WHERE ${data.where}` : '';
-//         db.all('SELECT * FROM ' + data.tableName + query, (err, rows) => {
-//           if (err) {
-//             console.error(err);
-//             return;
-//           }
-//           ws.send(
-//             JSON.stringify({
-//               type: 'databaseSelectAllChanged',
-//               tableName: data.tableName,
-//               value: rows,
-//             })
-//           );
-//         });
-//       } else if (type === 'selectAllTwo') {
-//         db.all('SELECT * FROM ' + data.tableName, (err, rows) => {
-//           if (err) {
-//             console.error(err);
-//             return;
-//           }
-//           ws.send(
-//             JSON.stringify({
-//               type: 'databaseSelectAllTwoChanged',
-//               tableName: data.tableName,
-//               value: rows,
-//             })
-//           );
-//         });
-//       } else if (type === 'selectAllunit') {
-//         db.all('SELECT * FROM ' + data.tableName, (err, rows) => {
-//           if (err) {
-//             console.error(err);
-//             return;
-//           }
-//           ws.send(
-//             JSON.stringify({
-//               type: 'databaseSelectAllunitChanged',
-//               tableName: data.tableName,
-//               value: rows,
-//             })
-//           );
-//         });
-//       } else if (type === 'selectSorted') {
-//         select(
-//           data.tableName,
-//           data.placeholders,
-//           data.where,
-//           data.orderBy,
-//           data.orderDirection
-//         ).then((rows) => {
-//           ws.send(
-//             JSON.stringify({
-//               type: 'databaseSelectSortedChanged',
-//               tableName: data.tableName,
-//               value: rows,
-//             })
-//           );
-//         });
-//       }
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   });
-// }
 
 export { open, close, control };
